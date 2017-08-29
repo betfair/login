effdbd61ac993e4fa192b70f99c956b70744ff10
Mike Bateman <mike@MikesMac.local>
2017-08-29 21:49:50 +0100

    Adding template files

--- dist/betfair-js-login.js
+++ dist/betfair-js-login.js
@@ -1,11 +1,10 @@
-import * as https from 'https';
-
+"use strict";
+exports.__esModule = true;
+var https = require("https");
 var username = 'XXXXXXXXX';
 var password = 'YYYYYYYYYY';
 var appkey = 'ZZZZZZZZZZZZ';
-
-
-const options = {
+var options = {
     hostname: 'identitysso.betfair.com',
     port: 443,
     path: '/api/login?username=' + username + '&password=' + password,
@@ -15,18 +14,14 @@ const options = {
         "Accept": "application/json"
     }
 };
-
-const req = https.request(options, (res) => {
+var req = https.request(options, function (res) {
     console.log('statusCode:', res.statusCode);
     console.log('headers:', res.headers);
-
-    res.on('data', (d) => {
+    res.on('data', function (d) {
         process.stdout.write(d);
     });
 });
-
-req.on('error', (e) => {
+req.on('error', function (e) {
     console.error(e);
 });
-
 req.end();
