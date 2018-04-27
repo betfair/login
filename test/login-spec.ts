b952bbb5ebfa20b64dbe7e0b420b5f164e1092df
Mike Bateman <mike.bateman@timeform.com>
2018-04-27 13:05:21 +0100

    More testing :-(

--- test/login-spec.ts
+++ test/login-spec.ts
@@ -8,34 +8,34 @@ dotenv.config();
 
 const expect = chai.expect;
 
-// describe("Login", () => {
-//   it("login me in", function (done) {
-//     var bfLogin = new Login(
-//       <string>process.env.BFUSERNAME,
-//       <string>process.env.BFPASSWORD,
-//       <string>process.env.BFAPPKEY);
+describe("Login", () => {
+  it("login me in", function (done) {
+    var bfLogin = new Login(
+      <string>process.env.BFUSERNAME,
+      <string>process.env.BFPASSWORD,
+      <string>process.env.BFAPPKEY);
 
-//     bfLogin.login().then(function (token) {
-//       expect(token).to.not.be.undefined;
-//       expect(token.length).to.be.greaterThan(10);
-//       done();
-//     });
-//   });
-// });
+    bfLogin.login().then(function (token) {
+      expect(token).to.not.be.undefined;
+      expect(token.length).to.be.greaterThan(10);
+      done();
+    });
+  });
+});
 
 describe("Varriables", () => {
   it("BFUSERNAME", () => {
-    expect(process.env.BFUSERNAME).to.not.be.undefined;
-    expect(process.env.BFUSERNAME).to.be.greaterThan(2);
+    expect(<string>process.env.BFUSERNAME).to.not.be.undefined;
+    expect(<string>process.env.BFUSERNAME).to.be.greaterThan(2);
   });
 
   it("BFPASSWORD", () => {
-    expect(process.env.BFPASSWORD).to.not.be.undefined;
-    expect(process.env.BFPASSWORD).to.be.greaterThan(2);
+    expect(<string>process.env.BFPASSWORD).to.not.be.undefined;
+    expect(<string>process.env.BFPASSWORD).to.be.greaterThan(2);
   });
 
   it("BFAPPKEY", () => {
-    expect(process.env.BFAPPKEY).to.not.be.undefined;
-    expect(process.env.BFAPPKEY).to.be.greaterThan(2);
+    expect(<string>process.env.BFAPPKEY).to.not.be.undefined;
+    expect(<string>process.env.BFAPPKEY).to.be.greaterThan(2);
   });
 });
